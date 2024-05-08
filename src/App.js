cecd6b5e156844b15a1698d3797229764c64a461
apf-dev <apf-dev@outlook.com>
2024-05-08 15:58:05 -0400

    Se agregan todas las funcionalidades.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { Buscador } from "./components/Buscador";
+import { Listado } from "./components/Listado";
+import { Crear } from "./components/Crear";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [listadoState, setListadoState] = useState([])
+
+    return (
+        <div className="layout">
+            {/* Cabecera */}
+            <header className="header">
+                <div className="logo">
+                    <div className="play"></div>
+                </div>
+                
+                <h1>MisLibros</h1>
+            </header>
+
+            {/** Barra de navegación */}
+            <nav className="nav">
+                <ul>
+                    <li><a href="/">Inicio</a></li>
+                    <li><a href="/">Libros</a></li>
+                    <li><a href="/">Contacto</a></li>
+                </ul>
+            </nav>
+
+            {/** Contenido principal */}
+            <section id="content" className="content">
+                <Listado listadoState={listadoState} setListadoState={setListadoState} />
+            </section>
+
+            {/** Barra lateral */}
+            <aside className="lateral">
+                <Buscador listadoState={listadoState} setListadoState={setListadoState} />
+
+                <Crear setListadoState={setListadoState} />
+            </aside>
+
+            {/**Pie de página*/}
+            <footer className="footer">
+                &copy; Pie de página
+            </footer>
+
+        </div>
+    );
 }
 
 export default App;
